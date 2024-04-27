9be45ba55b74a2bcfa79f651b011cccbafece48a
dartiros <realdarter1234@gmail.com>
2024-04-27 16:48:12 -0700

    Augmented Data Creation
    Data Augmentation

--- Data/run_verify_data.py
+++ Data/run_verify_data.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import os
 
-# Check if the file exists
-file_path = "english.csv"
+# Get the directory of the current script
+script_dir = os.path.dirname(__file__)
+
+# Define the file path relative to the script directory
+file_path = os.path.join(script_dir, "english.csv")
+
+toggle = True
+
 if not os.path.exists(file_path):
     print(f"File not found: {file_path}")
     # You might want to handle this case gracefully, e.g., exit the script or raise an error
@@ -15,9 +21,14 @@ else:
 
     # Verify if image files exist
     for index, row in df.iterrows():
-        image_path = row['image_path']
+        # Construct the full image path relative to the script directory
+        image_path = os.path.join(script_dir, row['image_path'])
         if not os.path.exists(image_path):
             print(f"Image file not found: {image_path}")
-    print("Data All Valid")
+            toggle = False
     # Display the first few rows of the dataframe
     print(df.head())
+
+
+print(f"Is All Data Valid: {toggle}")
+
